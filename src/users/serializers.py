df2a726467d26327815cb8cf7db80988782394c8
hounfodji <hospicehounfodji@gmail.com>
2024-06-13 15:36:14 +0100

    Setting Up User Registration

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -4,6 +4,18 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     
+    password = serializers.CharField(write_only=True, required=False)
+    
+    def create(self, validated_data):
+        print(validated_data)
+        password = validated_data.pop("password")
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        print(validated_data)
+        user.save()
+        
+        return user
+    
     class Meta:
         model = User
-        fields = ['url', 'id', 'email', 'username', 'first_name', 'last_name']
+        fields = ['url', 'id', 'email', 'username', 'first_name', 'last_name', 'password']
