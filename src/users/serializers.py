463938a51b453b0e6d16c4cbbd0df6bfa7d7dea3
hounfodji <hospicehounfodji@gmail.com>
2024-06-17 16:41:59 +0100

    User Serialiser Custom Validation

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -8,6 +8,18 @@ class UserSerializer(serializers.ModelSerializer):
     old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
     
+    def validate(self, data):
+        request_method = self.context['request'].method
+        password = data.get('password', None)
+        if request_method == 'POST':
+            if password == None:
+                raise serializers.ValidationError({"info": "Please provide a password."})
+        elif request_method == "PATCH" or request_method =="PUT":
+            old_password = data.get('old_password', None)
+            if password != None and old_password == None:
+                raise serializers.ValidationError({"info": "Please provide old password"})
+        return data
+    
     def create(self, validated_data):
         password = validated_data.pop("password")
         user = User.objects.create(**validated_data)
@@ -19,15 +31,16 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         try:
             user = instance
-            password = validated_data.pop("password")
-            old_password = validated_data.pop("old_password")
-            if user.check_password(old_password):
-                user.set_password(password)
-            else:
-                raise Exception("Old password is not correct")
-            user.save()
+            if "password" in validated_data:
+                password = validated_data.pop("password")
+                old_password = validated_data.pop("old_password")
+                if user.check_password(old_password):
+                    user.set_password(password)
+                else:
+                    raise Exception("Old password is incorrect")
+                user.save()
         except Exception as err:
-            raise serializers.ValidationError(err)
+            raise serializers.ValidationError({"info": err})
         return super(UserSerializer, self).update(instance, validated_data)
     
     class Meta:
